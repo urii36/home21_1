bc188f6c2e48c9a1e2b2bb5051e22d0dbcf5fd3d
urii36 <kazan313131@gmail.com>
2023-10-09 17:47:33 +0300

    домашка 22.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,5 +8,5 @@ app_name = 'catalog'
 urlpatterns = [
     path('', ProductListView.as_view(), name='home_page'),
     path('contacts/', contacts, name='contacts'),
-    path('product/<pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
