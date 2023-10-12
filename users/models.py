d0ee36683aba4e2fe9377d7849746d16ea695333
urii36 <kazan313131@gmail.com>
2023-10-12 13:35:13 +0300

    домашка 22.2

--- users/models.py
+++ users/models.py
@@ -14,6 +14,7 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     country = models.CharField(max_length=150, verbose_name='страна')
+    is_verified_email = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
